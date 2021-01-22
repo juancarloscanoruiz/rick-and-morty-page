6cd897c7d09ef41b14bfe8154adb29e0b7a6baaa
Juan Carlos <juancarloscanoruiz7@gmail.com>
2021-01-22 00:14:08 -0600

    transición en campo de búsqueda

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -5,7 +5,7 @@ import SearchIcon from "../../assets/images/search-icon.svg"
 
 export default function Input({ placeholder, onChange }){
     return (
-        <div className="d-flex align-items-center search-field mb-3 ml-2 mr-2 pt-2 pb-2 pl-3 pr-3">
+        <div className="d-flex align-items-center search-field mb-3 ml-2 mr-2 pt-3 pb-3 pl-3 pr-3">
             <img src={SearchIcon} alt="Search Icon"/>
             <input type="text" className="ml-2 text-normal search-field__input text-regular-medium-neutral1" placeholder={placeholder} onChange={(e) => onChange(e)} />
         </div>
