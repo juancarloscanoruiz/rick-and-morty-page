b7a068c470be189e60f0204d889570346234b287
Juan Carlos <juancarloscanoruiz7@gmail.com>
2021-01-22 00:54:09 -0600

    cambio a hash router

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import React from "react";
-import { Switch, BrowserRouter as Router, Route } from "react-router-dom"
+import { Switch, HashRouter as Router, Route } from "react-router-dom"
 import Home from "./pages/Home";
 import Character from "./pages/Character";
 import "./App.scss"
 function App() {
 
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>
       <Switch>
         <Route path="/" exact="true" component={Home}/>
         <Route path="/character/:id" exact="true" component={Character}/>
