c4a5b09227c8e95ac394683faf9f7f107df4e038
Juan Carlos <juancarloscanoruiz7@gmail.com>
2021-01-22 01:07:34 -0600

    cambio de title

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -5,7 +5,9 @@ import CardList from "../../components/CardList";
 export default function Home() {
 
   return (
+    
     <div className="d-flex justify-content-center container">
+      
       <div className="col-10">
         <h1 className="text-get-schwifty-primary h1 text-center mt-5 mb-5">Rick and Morty</h1>
         <CardList />
